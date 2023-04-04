11398713d485a60eb703b2a502049cfafd3ff802
JonathanOSC <jonathanfelipeochoasilva@gmail.com>
2023-04-03 21:18:51 -0500

    Third-Class

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -9,6 +9,7 @@ class Dinosaur(Sprite):
     X_POS = 80
     Y_POS = 310
     JUMP_SPEED = 8.5
+    Y_POS_DUCK = 340
 
     def __init__(self):
         self.image = RUNNING[0]
@@ -32,14 +33,13 @@ class Dinosaur(Sprite):
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_run = False
             self.dino_jump = True
-        elif not self.dino_jump:
-            self.dino_run = True
-
-        if user_input[pygame.K_DOWN]:
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_jump = False
             self.dino_run = False
             self.dino_duck = True
-        elif not self.dino_duck:
-            self.dino_duck = True
+        elif not self.dino_jump:
+            self.dino_duck = False
+            self.dino_run = True
         
 
         if self.step_index >= 10:
@@ -66,7 +66,7 @@ class Dinosaur(Sprite):
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS 
-        self.dino_rect.y = self.Y_POS + 35
+        self.dino_rect.y = self.Y_POS_DUCK
         self.step_index += 1
 
     def draw(self, screen):
